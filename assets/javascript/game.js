425ad46093a0c1dd47f918c6f2b95e0d8067747d
Dangggchris <christophermtdang@gmail.com>
2019-03-25 17:04:17 -0700

    final code, i think

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+var validInputs = "abcdefghijklmnopqrstuvwxyz"; 
+var userInput = [];
+var computerGuess = validInputs[Math.floor(Math.random() * validInputs.length)]; // Generates random letter
+var win = 0; 
+var loss = 0;
+var guesses = 15;
+
+console.log(computerGuess);
+
+document.onkeydown = function (input) {
+    var userGuess = event.key; 
+
+    //if user guess is correct
+    if (userGuess === computerGuess) {
+        win++; 
+        guesses = 15;
+        loss = 0;
+    }
+
+    //if user guess is incorrect
+    else if (userGuess !== computerGuess) {
+        guesses--;
+        
+        if (userInput.includes(userGuess)) {
+            alert("Letter already tried");
+        }
+        userInput.push(userGuess);
+    }
+
+    //when user uses all guesses
+    if (guesses === 0) {
+        loss++;
+        guesses = 15;
+    }
+    
+document.getElementById("wins").textContent = win;
+document.getElementById("loss").textContent = loss;
+document.getElementById("guesses").textContent = guesses;
+document.getElementById("tried").textContent = userInput;
+
+
+console.log(userGuess);
+
+}
+
